bdc7996b9ed39b8fb2e898b2e10462a9a9fe6450
David Van Der Beek <earlynovrock@gmail.com>
2016-03-28 09:03:43 -0500

    add loan.payments

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,9 +1,13 @@
 class Loan < ActiveRecord::Base
   has_many :payment_schedules, dependent: :destroy
 
-  delegate :amount_due_cents, :amount_due_dollars, :interest_rate, :payments, to: :payment_schedule
+  delegate :amount_due_cents, :amount_due_dollars, :interest_rate, to: :payment_schedule
 
   def payment_schedule(as_of: Date.current)
     payment_schedules.current(as_of: as_of)
+  end
+
+  def payments(as_of: Date.current)
+    payment_schedule(as_of: as_of).try(:payments)
   end
 end
